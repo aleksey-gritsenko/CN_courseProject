e8fb2a948f7d8fe4308d85f8aa72c850cdb65066
Тимур <tejmurrasulov@gmail.com>
2019-06-03 00:00:43 +0300

    response in mouseListener

--- Project/src/po/client/mousePressedListener.java
+++ Project/src/po/client/mousePressedListener.java
@@ -24,6 +24,18 @@ public class mousePressedListener implements MouseListener {
         System.out.println("x = " + x+"!!");
         System.out.println("y = " + y+"!!");
     }
+    public static String toString (int i){
+        String numberAsString = Integer.toString (i);
+        return numberAsString;
+    }
+    public String Response(MouseEvent e){
+        Point location = MouseInfo.getPointerInfo().getLocation();
+        int x =(int) location.getX();
+        int y =(int) location.getY();
+        String response = toString(x) + "." + toString(y);
+        return  response;
+
+    }
 
     public void mouseReleased(MouseEvent e) {
     }
